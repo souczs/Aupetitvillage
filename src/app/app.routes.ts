d671d3caeb59b8928e432b52222fa5a668a6fad2
Sonia EL BOURI <soniaelbouri@Mac-001.lan>
2025-07-26 13:10:05 +0200

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Product } from './product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: 'product/:id', component: Product }
+];
