9c09631b809146fe243335577f4528f0aea442f7
Sonia EL BOURI <soniaelbouri@Mac.lan>
2025-07-27 17:41:50 +0200

    header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { About } from "../about/about";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule, About],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-
+  menuOpen = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
